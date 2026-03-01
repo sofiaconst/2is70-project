491cb35f2b077f24d702e38bb7f94e64786f1991
busego06 <bugra.karac@gmail.com>
2026-03-01 15:07:42 +0100

    Added Tab Layout to FeedFragment

--- EduView/app/src/main/java/com/example/eduview/FeedFragment.java
+++ EduView/app/src/main/java/com/example/eduview/FeedFragment.java
@@ -27,16 +27,6 @@ public class FeedFragment extends Fragment {
     public FeedFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FeedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FeedFragment newInstance(String param1, String param2) {
         FeedFragment fragment = new FeedFragment();
         Bundle args = new Bundle();
