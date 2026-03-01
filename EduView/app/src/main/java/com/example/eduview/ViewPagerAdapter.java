3e4b6f7f947b5f26809205ae3d77e02c1593101f
busego06 <bugra.karac@gmail.com>
2026-03-01 11:58:53 +0100

    Added tabs to Feed UI

---
+++ EduView/app/src/main/java/com/example/eduview/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.example.eduview;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: return new Posts();
+            case 1: return new Announcements();
+            // add if statement for teacher
+            case 2: return new Pending();
+            default: return new Posts();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
