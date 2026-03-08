a50e76a9d1a60f290da8b6e5625844802594f697
Leo Benitah <l.f.benitah@student.tue.nl>
2026-03-08 14:17:55 +0100

    Testing #4

--- EduView/app/src/androidTest/java/com/example/eduview/ExampleInstrumentedTest.java
+++ EduView/app/src/androidTest/java/com/example/eduview/ExampleInstrumentedTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.google.firebase.database.collection.BuildConfig;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -17,10 +19,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.eduview", appContext.getPackageName());
-    }
+//    @Test
+//    public void useAppContext() {
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//        assertEquals(BuildConfig.APPLICATION_ID, appContext.getPackageName());
+//    }
 }
