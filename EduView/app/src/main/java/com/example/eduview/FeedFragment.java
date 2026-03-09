cb74ebf278b6a197e30f7712bd472b87adfe0e61
busego06 <bugra.karac@gmail.com>
2026-03-09 17:26:03 +0300

    Moving in feed works, some work needed on how it looks

--- EduView/app/src/main/java/com/example/eduview/FeedFragment.java
+++ EduView/app/src/main/java/com/example/eduview/FeedFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,23 +33,26 @@ public class FeedFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FeedFragment","OnCreate");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("FeedFragment","onCreateView is created");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        Log.d("FeedFragment","View is created");
         TabLayout teacherTabs = view.findViewById(R.id.TeacherTabs);
         ViewPager2 viewPager = view.findViewById(R.id.viewPager);
 
-        viewPager.setAdapter(new ViewPagerAdapter(requireActivity()));
+        viewPager.setAdapter(new ViewPagerAdapter(this));
 
         new TabLayoutMediator(teacherTabs, viewPager, (tab, position) -> {
             if (position == 0) tab.setText(getString(R.string.feed_title_1)); // Posts
