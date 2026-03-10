d2bd7db699722b829a9bea82ad9b98f88ea3dc8c
alexchristou06 <alexchristou2006@gmail.com>
2026-03-10 19:17:31 +0100

    FIxed merge conflicts

--- EduView/app/build.gradle.kts
+++ EduView/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.google.gms.google.services) // Add this line
     // Kotlin serialization plugin for type safe routes and navigation arguments
     kotlin("plugin.serialization") version "2.0.21"
     alias(libs.plugins.kotlin.compose)
