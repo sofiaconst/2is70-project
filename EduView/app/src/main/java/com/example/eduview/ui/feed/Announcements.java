e178ebe53ba9e867a32f8ba421da4beb05a5984d
busego06 <bugra.karac@gmail.com>
2026-03-15 17:57:14 +0300

    Final improvements and fixed feed classes to have no errors

--- EduView/app/src/main/java/com/example/eduview/ui/feed/Announcements.java
+++ EduView/app/src/main/java/com/example/eduview/ui/feed/Announcements.java
@@ -1,4 +1,4 @@
-package com.example.eduview;
+package com.example.eduview.ui.feed;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.eduview.R;
 
 public class Announcements extends Fragment {
     public Announcements() {
