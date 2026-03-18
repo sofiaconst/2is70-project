25af98f5979069c8891188eab1288a4bce17eb0a
alexchristou06 <alexchristou2006@gmail.com>
2026-03-18 22:17:18 +0100

    Implemented the reading of the QR code (WAS NOT POSSIBLE TO TEST!!!) and made changes to sign up - App must be able to read QR codes & connect students to the classroom - Sign Up can now only happen with unique emails - Chnaged add and remove children buttons

---
+++ EduView/app/src/main/java/com/example/eduview/ui/profile/CustomScannerActivity.java
@@ -0,0 +1,54 @@
+package com.example.eduview.ui.profile;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.eduview.R;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+public class CustomScannerActivity extends AppCompatActivity {
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_scanner);
+
+        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner);
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.setShowMissingCameraPermissionDialog(false);
+        capture.decode();
+
+        ImageButton btnBack = findViewById(R.id.btn_scanner_back);
+        btnBack.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+}
